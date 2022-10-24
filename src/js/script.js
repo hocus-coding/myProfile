982df6c62600ee91bf245d94c34103e3702c124c
Dmitry <lysogordmitry@gmail.com>
2022-10-24 20:16:36 +0300

    анимация загрузки, бг для фреймов, смена темы инпутов

--- src/js/script.js
+++ src/js/script.js
@@ -26,11 +26,6 @@ window.onscroll = function () {
   let scrollH = page.scrollHeight;
   let scrolled = page.scrollHeight - top;
 
-  // console.log(scrollH + ' скрол высота');
-  // console.log(top + ' скрол топ');
-  // console.log(pageHeight + ' клиент высота');
-  // console.log(scrolled + ' сколько осталось скрола');
-
   function comparison() {
 
     let arr = [
@@ -123,5 +118,5 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 });
 
-/* SmtpJS.com - v3.0.0 */
+/* Email send SmtpJS.com - v3.0.0 */
 var Email = { send: function (a) { return new Promise(function (n, e) { a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a); Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) }) }) }, ajaxPost: function (e, n, t) { var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n) }, ajax: function (e, n) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send() }, createCORSRequest: function (e, n) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t } };
